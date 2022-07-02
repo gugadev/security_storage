4116cb455ecf8aa64005178f8c6da18d6244dae8
Gustavo García <igugadev@gmail.com>
2022-07-02 18:25:45 -0500

    feat(partially): add empty main activity

---
+++ android/src/main/kotlin/com/pacificoseguros/securitystorage/security_storage/MainActivity.kt
@@ -0,0 +1,6 @@
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin
+
+class MainActivity :
+    FlutterActivity() {}
