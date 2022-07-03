f1f47ca9e8f7bc0ef0329c360b1dd3d7bef46f77
Gustavo García <igugadev@gmail.com>
2022-07-02 19:00:09 -0500

    feat(partially): add applicationId and applicationName

--- android/src/main/kotlin/com/pacificoseguros/securitystorage/security_storage/MainActivity.kt
+++ android/src/main/kotlin/com/pacificoseguros/securitystorage/security_storage/MainActivity.kt
@@ -1,3 +1,4 @@
+package com.pacificoseguros.securitystorage.security_storage
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.firebase.core.FirebaseCorePlugin
