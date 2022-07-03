a975f6b4e594a05daea706272a08f966723c9f6d
Gustavo García <igugadev@gmail.com>
2022-07-02 21:49:53 -0500

    feat: fix MainActivity

--- example/android/app/src/main/kotlin/com/pacificoseguros/securitystorage/security_storage_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/pacificoseguros/securitystorage/security_storage_example/MainActivity.kt
@@ -1,18 +1,7 @@
 package com.pacificoseguros.securitystorage.security_storage_example
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class MainActivity: FlutterActivity() {
 
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
 }
